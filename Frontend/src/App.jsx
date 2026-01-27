7c5349cd3afa8456fd11800b177322f6ee59f3d8
mokib08 <mokib08@gmail.com>
2026-01-27 07:25:40 +0600

    some change in Note project

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -12,9 +12,10 @@ const App = () => {
 
 
       const copyTask = [...task];
-      copyTask.push(title, details)
+      copyTask.push({title, details})
       setTask(copyTask)
 
+      console.log(copyTask);
       console.log(task);
       
       setTitle('')
@@ -23,7 +24,7 @@ const App = () => {
 
   return (
     <div  className='h-screen lg:flex bg-black text-white  p-10 '>
-      <form onSubmit={(e) => submitHandler(e)}  className='lg:w-1/2 flex flex-col gap-5 p-10  items-start'>
+      <form onSubmit={(e) => {submitHandler(e)}}  className='lg:w-1/2 flex flex-col gap-5 p-10  items-start'>
         <h1 className='text-2xl font-bold'>Add Your Notes</h1>
         <input 
           onChange={(e) => {
@@ -49,18 +50,18 @@ const App = () => {
            <button className='w-full outline-none bg-white text-black px-5 py-2 rounded '>Add Note</button>
       </form>
 
-
       <div className=' lg:w-1/2 p-10'>
         <h1 className='text-xl font-bold'>Your Notes</h1>
         <div className='flex flex-wrap items-start justify-start gap-5 mt-5 h-full overflow-auto'>
           {task.map(function(elem, idx){
             return <div key={idx} className='h-52 w-40 rounded-xl py-10 px-4 text-black bg-cover bg-[url(https://imgs.search.brave.com/jC6eAW-2rxbieqD49n_B9KBqPopJoWA_8guYiuWBeE8/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wMTEv/NDIxLzI5NS9zbWFs/bC92ZXJ0aWNhbC1z/cGlyYWwtbm90ZWJv/b2stcG5nLnBuZw)]'>
-              <h3 className='leading-tight  text-xl font-bold '>{elem.title}</h3>
-              <p className='mt-2 leading-tight font-medium text-gray-700'>{elem.details}</p>
+                  <h3 className='leading-tight text-xl font-bold '>{elem.title}</h3>
+                  <p className='mt-2 leading-tight font-medium text-gray-700'>{elem.details}</p>
             </div>
           })}
         </div>
       </div>
+      
     </div>
   )
 }
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,8 +4,8 @@ import './index.css'
 import App from './App'
 
 
+
 createRoot(document.getElementById('root')).render(
-   
-      <App />
+<App />
 
 )
