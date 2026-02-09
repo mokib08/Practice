2dd969fbab823f761c8c9d2a56e17e0b6715eb4c
mokib08 <mokib08@gmail.com>
2026-02-09 22:34:45 +0600

    Today learn lifecycle component and API integration

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,8 +1,24 @@
+import axios from '../utils/axios'
 import React from 'react'
 
 const Home = () => {
+
+  const getProduct = async () => {
+    try {
+      const { data } = await axios.get("/products")
+      console.log(data);
+      
+    } catch (error) {
+      console.log(" this is axios error", error);
+      
+    }
+  }
+
   return (
-    <div>Home</div>
+    <div>
+      <h1>Home</h1>
+      <button onClick={getProduct}>Get Data</button>
+    </div>
   )
 }
 
